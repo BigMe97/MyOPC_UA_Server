5b67ab182883b9a1b7df24b1f677ff7c25365d87
BigMe97 <magnuseide@msn.com>
2023-04-24 12:05:43 +0200

    Add project files.

---
+++ OPCServer/OPCServerApplication.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace OPCServer
+{
+    partial class OPCServerApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbVariables = new System.Windows.Forms.ListBox();
+            this.lbValues = new System.Windows.Forms.ListBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.txtValidate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbVariables
+            // 
+            this.lbVariables.FormattingEnabled = true;
+            this.lbVariables.Location = new System.Drawing.Point(55, 84);
+            this.lbVariables.Name = "lbVariables";
+            this.lbVariables.Size = new System.Drawing.Size(144, 147);
+            this.lbVariables.TabIndex = 0;
+            // 
+            // lbValues
+            // 
+            this.lbValues.FormattingEnabled = true;
+            this.lbValues.Location = new System.Drawing.Point(204, 84);
+            this.lbValues.Name = "lbValues";
+            this.lbValues.Size = new System.Drawing.Size(123, 147);
+            this.lbValues.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Variables";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Values";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Enabled = false;
+            this.txtURL.Location = new System.Drawing.Point(55, 35);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(272, 20);
+            this.txtURL.TabIndex = 4;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.BackColor = System.Drawing.Color.Green;
+            this.btnStartStop.Location = new System.Drawing.Point(207, 250);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(120, 62);
+            this.btnStartStop.TabIndex = 5;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = false;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // txtValidate
+            // 
+            this.txtValidate.Location = new System.Drawing.Point(57, 249);
+            this.txtValidate.Name = "txtValidate";
+            this.txtValidate.Size = new System.Drawing.Size(129, 20);
+            this.txtValidate.TabIndex = 6;
+            // 
+            // OPCServerApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 380);
+            this.Controls.Add(this.txtValidate);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbValues);
+            this.Controls.Add(this.lbVariables);
+            this.Name = "OPCServerApplication";
+            this.Text = "OPC Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OPCServerApplication_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbVariables;
+        private System.Windows.Forms.ListBox lbValues;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.TextBox txtValidate;
+    }
+}
+
---
+++ OPCServer/OPCServerApplication.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using Opc.UaFx.Server;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace OPCServer
+{
+    public partial class OPCServerApplication : Form
+    {
+        OpcServer server;
+        
+        string configFile = String.Format("{0}\\Configuration.xml", Directory.GetParent(Environment.CurrentDirectory).Parent.FullName);
+        string address;
+
+        public OPCServerApplication()
+        {
+            InitializeComponent();
+
+            GetConfiguration(configFile);
+            server.Start();
+        }
+
+        public void GetConfiguration(string filePath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+            XmlReader reader = XmlReader.Create(filePath);
+            XmlNode node = doc.SelectSingleNode("CONFIG/address");
+            address = Convert.ToString(node.InnerText);
+            txtURL.Text = address;
+
+            lbVariables.Items.Clear();
+            List<OpcDataVariableNode> nodes = new List<OpcDataVariableNode>();
+            while (reader.Read())
+            {
+                if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "item"))
+                {
+                    if (reader.HasAttributes)
+                    {
+                        string name = reader.GetAttribute("name");
+                        string typ = reader.GetAttribute("type");
+                        lbVariables.Items.Add(name);
+                        txtValidate.Text = typ;
+                        if (typ == "double")
+                        {
+                            nodes.Add(new OpcDataVariableNode<double>(name));
+                        }
+                        else if (typ == "int")
+                        {
+                            nodes.Add(new OpcDataVariableNode<int>(name));
+                        }
+                        else if (typ == "string")
+                        {
+                            nodes.Add(new OpcDataVariableNode<string>(name));
+                        }
+                        else
+                        {
+                            nodes.Add(new OpcDataVariableNode(name));
+
+                        }
+                    }
+                }
+            }
+            XmlNodeList items = doc.SelectNodes("CONFIG/item");
+            
+            foreach (XmlNode item in items)
+            {
+                
+            }
+            
+            server = new OpcServer(address, nodes);
+
+        }
+
+       
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (btnStartStop.Text == "Start")
+            {
+                lbValues.Items.Clear();
+            }
+            else
+            {
+                var client = new OpcClient(address);
+                client.Connect();
+                lbValues.Items.Clear();
+                foreach (var item in lbVariables.Items)
+                {
+                    lbValues.Items.Add(client.ReadNode(String.Format("ns=2;s={0}", item.ToString())));
+                }
+                client.Disconnect();
+            }
+        }
+
+        private void OPCServerApplication_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            server.Stop();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (btnStartStop.Text == "Start")
+            {
+                server.Stop();
+                btnStartStop.Text = "Stop";
+                txtURL.Enabled = false;
+                GetConfiguration(configFile);
+                server.Start();
+                btnStartStop.BackColor = Color.Red;
+
+            }
+            else
+            {
+                btnStartStop.Text = "Start";
+                btnStartStop.BackColor = Color.Green;
+                txtURL.Enabled = true;
+                server.Stop();
+            }
+        }
+    }
+}
